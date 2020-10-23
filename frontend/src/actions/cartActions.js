d4c711cbf098f684dd2007b86533ea6fb6998210
blasdc <blas.decor@gmail.com>
2020-10-23 17:53:37 +0200

    PlanShopFull V1.3

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,7 +2,9 @@ import Axios from 'axios';
 import Cookie from 'js-cookie';
 import { 
     CART_ADD_ITEM,
-    CART_REMOVE_ITEM
+    CART_REMOVE_ITEM,
+    CART_SAVE_PAYMENT,
+    CART_SAVE_SHIPPING
  } from "../constants/cartConstants";
 
 
@@ -34,4 +36,12 @@ const removeFromCart = (productId) => async (dispatch, getState) => {
     Cookie.set("cartItems", JSON.stringify(cartItems)); 
 }
 
-export { addToCart, removeFromCart }
+const saveShipping = (data) => (dispatch) => {
+    dispatch({ type: CART_SAVE_SHIPPING, payload: data })
+}
+
+const savePayment = (data) => (dispatch) => {
+    dispatch({ type: CART_SAVE_PAYMENT, payload: data })
+}
+
+export { addToCart, removeFromCart, saveShipping, savePayment }
